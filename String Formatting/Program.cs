704db998e450ae10216f7e0ad67dad8443489871
Justin <judecabodil22@gmail.com>
2024-04-19 19:00:49 +0800

    Debugging

--- String Formatting/Program.cs
+++ String Formatting/Program.cs
@@ -1,4 +1,4 @@
-﻿/*
+﻿
 //String Formatting testing
 
 string first = "Hello";
@@ -29,10 +29,10 @@ Console.WriteLine(input.PadRight(12));
 Console.WriteLine(input.PadLeft(12, '-'));
 Console.WriteLine(input.PadRight(12, '-'));
 Console.WriteLine(name.PadLeft(12, '-'));
-*/
+
 
 //Working with padded strings
-/*string paymentId = "769C";
+string paymentId = "769C";
 string payeeName = "Mr. Stephen Ortega";
 string paymentAmount = "$5,000.00";
 
@@ -41,6 +41,6 @@ formattedLine += payeeName.PadRight(24);
 formattedLine += paymentAmount.PadLeft(10);
 Console.WriteLine("1234567890123456789012345678901234567890");
 Console.WriteLine(formattedLine);
-*/
+
 
 
